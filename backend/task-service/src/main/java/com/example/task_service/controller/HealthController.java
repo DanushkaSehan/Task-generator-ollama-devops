85a5a06d117b3ebd4ab98023fc38c230149d46bb
DanushkaSehan <39-bce-0013@kdu.ac.lk>
2025-11-06 11:26:00 +0530

    updated pipeline

---
+++ backend/task-service/src/main/java/com/example/task_service/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.task_service.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
