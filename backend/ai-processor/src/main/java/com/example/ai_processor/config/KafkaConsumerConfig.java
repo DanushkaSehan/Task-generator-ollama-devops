710b51db111e5c293b676928e6dd42dec9f0c4e2
DanushkaSehan <39-bce-0013@kdu.ac.lk>
2025-11-10 13:19:22 +0530

    kafka updated

--- backend/ai-processor/src/main/java/com/example/ai_processor/config/KafkaConsumerConfig.java
+++ backend/ai-processor/src/main/java/com/example/ai_processor/config/KafkaConsumerConfig.java
@@ -20,9 +20,9 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, Task> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "34.63.78.54:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "35.202.103.172:9092");
         // props.put(ConsumerConfig.GROUP_ID_CONFIG, "ai-processor-vm");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "ai-processor");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "ai-processor-vm");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.example.aiprocessor.model");
